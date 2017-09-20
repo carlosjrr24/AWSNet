610d5f8599b7bfe90e9c3662cd206a728217b40c
Carlos Rosales <carlosjrr@hotmail.com>
2017-09-20 17:14:46 -0300

    some modification in repository pattern

---
+++ src/AWSNet.Model/Entities/Products.cs
@@ -0,0 +1,46 @@
+﻿using AWSNet.Model.Base; //revisar x q no esta en publishing stattus
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AWSNet.Model.Entities
+{
+    public class Products : Entity
+    {
+        public int      ID { get; set; }
+        public string   Name { get; set; }
+        public int      CategoryID { get; set; }
+        public decimal  UnitPrice { get; set; }
+        public int      UnitsStock { get; set; }
+        public bool     IsDeleted { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Nullable<DateTime> LastModificationDate { get; set; }
+
+
+        public Products()
+        {
+            ID = 0;
+            Name = string.Empty;
+            CategoryID = 0;
+            UnitPrice = 0;
+            UnitsStock = 0;
+
+            IsDeleted = false;
+
+            CreationDate = DateTime.UtcNow;
+            LastModificationDate = DateTime.UtcNow;
+        }
+
+        public void Delete()
+        {
+            this.IsDeleted = true;
+            this.LastModificationDate = DateTime.UtcNow;
+        }
+
+        public void Restore()
+        {
+            this.IsDeleted = false;
+            this.LastModificationDate = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/AWSNet.Model/ModelContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Microsoft;
+using AWSNet.Model.Entities;
+
+namespace AWSNet.Model
+{
+    public class ModelContext : DbContext
+    {
+        public virtual DbSet<Product> Product { get; set; }
+
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options)
+        { }
+    }
+}
